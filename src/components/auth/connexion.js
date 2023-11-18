a61072c13a185ed5140c460d1ac54e2922e4f135
safiylm <121391404+safiylm@users.noreply.github.com>
2023-11-19 00:33:29 +0100

    update connexion page frontend

--- src/components/auth/connexion.js
+++ src/components/auth/connexion.js
@@ -1,12 +1,21 @@
+import '../../styles/connexion.css'
+
+
 
 function Connexion() {
   
     return ( 
-      <div className="">
-          <input placeholder="email" className=""></input>
-          <input type="password" placeholder="password" className=""></input>
+      <div id="page-connexion">
+      <div id="div-connexion">
+        <form>
+          <label>Email : </label><input type="text" placeholder="Saisir son e-mail" className="" />
+          <label>Password : </label><input type="password" placeholder="Saisir son mot de passe " className=""/>
+          <button type="button">Se connecter </button>
+          </form>
+      </div>
       </div>
     );
+
   }
   
   export default Connexion;
