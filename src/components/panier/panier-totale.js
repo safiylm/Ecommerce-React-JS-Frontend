cb9afcf1920a184279fed013d6e8c24d3b0bd595
safiylm <121391404+safiylm@users.noreply.github.com>
2023-11-20 21:19:18 +0100

    update panier somme totale

--- src/components/panier/panier-totale.js
+++ src/components/panier/panier-totale.js
@@ -2,37 +2,45 @@ import '../../styles/Panier/panier-totale.css'
 import React, { useEffect, useState } from "react";
 import { liste_produits } from '../liste-produit/array-liste-produit'
 
-function calculeSommeTotale (){
+function calculeSommeTotale(panier) {
 
+    let t = 0;
+    panier.map((panier_index) =>
+        liste_produits.filter(prod => prod.id == panier_index).map((prod) =>
+            t = t + prod.prix
+        )
+    )
+    
+
+    return t;
 }
 
 
-
 function PanierTotale({ panier, numberArticle, setNumberArticle }) {
-    const [sommeTotal, setSommeTotale] = useState(0)
+    const sommeTotal = calculeSommeTotale(panier)
     return (
 
         <div className="div-total-paiement">
             <h4>TOTAL</h4>
             <div className="somme">
                 <div>Sous-total</div>
-                {panier.length <= 0 ? <div> 0.00 $ </div> :
+                {panier.length <= 0 ? <div> 0.00 € </div> :
 
                     panier.map((panier_index) =>
                         liste_produits.filter(prod => prod.id == panier_index).map((prod) =>
-                       <div>{prod.prix}</div>
+                            <div style={{ "display": "none"}}>{  (Math.round(prod.prix * 100) / 100).toFixed(2) }</div>
                         ))
                 }
-                <div>  {sommeTotal } $ </div>
-            </div>
+               <div>  <strong>{(Math.round( sommeTotal* 100) / 100).toFixed(2)} € </strong></div>
+            </div> 
+
             <div className="livraison-x">
                 <div>Livraison</div>
                 <select>
                     <option>Livraison Standard en Point de relais ...</option>
                 </select>
             </div>
-
-            <button onClick={calculeSommeTotale()} id="paiement-btn">PAIEMENT</button>
+            <button  id="paiement-btn">PAIEMENT</button>
             {/* <div>
                 NOUS ACCEPTIONS:
             </div>
--- src/components/panier/panier-un-article.js
+++ src/components/panier/panier-un-article.js
@@ -1,45 +1,50 @@
 import '../../styles/Panier/panier-un-article.css'
 import { Outlet, Link } from "react-router-dom";
+import { liste_produits } from '../liste-produit/array-liste-produit'
 
-function UnArticle({ numberArticle, setNumberArticle }) {
+function UnArticle({ id_produit, setNumberArticle }) {
 
     return (
+        <>
+            {
+                liste_produits.filter(prod => prod.id == id_produit).map((prod) =>
 
-        <div className='div-un-article'>
-            <img src="https://img01.ztat.net/article/spp-media-p1/397fd247583d49adbc68ae58a24a4c23/b8d1b875cf974fe7af40aa28246f977b.jpg?filter=packshot" alt="produit" />
+                    <div className='div-un-article' key={prod.id} >
+                        <img src={prod.image_url[0]} alt="produit" />
 
-            <div className="article-info">
-                <Link to="/produit">
-                    <p>Escarpins à talons hauts</p>
-                    <p>49,99$</p>
-                </Link>
-                <div className="div-taille">
-                    <div> Taille &nbsp;&nbsp;</div>
-                    <select>
-                        <option>EU 36</option>
-                        <option>EU 38</option>
-                        <option>EU 40</option>
-                        <option>EU 42</option>
-                    </select>
-                </div>
+                        <div className="article-info">
+                            <Link to={"/produit?id="+prod.id}>
+                                <p>{prod.nom}</p>
+                                <p>{prod.prix} €</p>
+                            </Link>
+                            <div className="div-taille">
+                                <div> Taille &nbsp;&nbsp;</div>
+                                <select>
+                                    <option>EU 36</option>
+                                    <option>EU 38</option>
+                                    <option>EU 40</option>
+                                    <option>EU 42</option>
+                                </select>
+                            </div>
 
-                <div className="div-quantite">
-                    <div> Quantité &nbsp;&nbsp;</div>
-                    <select>
-                        <option onClick={() => setNumberArticle(1)}> 1</option>
-                        <option onClick={() => setNumberArticle(2)}> 2</option>
-                        <option onClick={() => setNumberArticle(3)}> 3</option>
-                        <option onClick={() => setNumberArticle(4)}> 4</option>
-                        <option onClick={() => setNumberArticle(5)}> 5</option>
-                    </select>
-                </div>
-                <button>Sauvegarder pour plus tard </button>
-            </div>
-        </div>
+                            <div className="div-quantite">
+                                <div> Quantité &nbsp;&nbsp;</div>
+                                <select>
+                                    <option onClick={() => setNumberArticle(1)}> 1</option>
+                                    <option onClick={() => setNumberArticle(2)}> 2</option>
+                                    <option onClick={() => setNumberArticle(3)}> 3</option>
+                                    <option onClick={() => setNumberArticle(4)}> 4</option>
+                                    <option onClick={() => setNumberArticle(5)}> 5</option>
+                                </select>
+                            </div>
+                            
+                        </div>
+                    </div>
 
-
-
+                )}
+        </>
     );
+
 }
 
 export default UnArticle;
