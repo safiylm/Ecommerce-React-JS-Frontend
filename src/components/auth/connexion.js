6e79cea62feaebc068b436d062c28c68f650b571
safiylm <121391404+safiylm@users.noreply.github.com>
2024-05-30 16:30:40 +0200

    update authentifiaction frontend

--- src/components/auth/connexion.js
+++ src/components/auth/connexion.js
@@ -8,8 +8,8 @@ function Connexion() {
       <div id="page-connexion">
       <div id="div-connexion">
         <form>
-          <label>Email : </label><input type="text" placeholder="Saisir son e-mail" className="" autocomplete="off" />
-          <label>Password : </label><input type="password" placeholder="Saisir son mot de passe " className="" autocomplete="off" />
+          <label>Email : </label><input type="text" placeholder="Saisir son e-mail" className="form-control" autocomplete="off" />
+          <label>Password : </label><input type="password" placeholder="Saisir son mot de passe " className="form-control" autocomplete="off" />
           <button type="button">Se connecter </button>
           <a id="go_page_inscription" href="/inscription"> S'inscrire </a>
           </form>
--- src/components/auth/inscription.js
+++ src/components/auth/inscription.js
@@ -8,10 +8,10 @@ function Inscription() {
       <div id="page-connexion">
       <div id="div-connexion">
         <form>
-          <label>Prénom : </label><input type="text" name="prenom" placeholder="Saisir son prénom" className="" autocomplete="off" />
-          <label>Nom : </label><input type="text" name="nom" placeholder="Saisir son nom" className="" autocomplete="off"  />
-          <label>Email : </label><input type="text" name="email" placeholder="Saisir son e-mail" className="" autocomplete="off" />
-          <label>Password : </label><input type="password" name="password" placeholder="Saisir son mot de passe " className="" autocomplete="off" />
+          <label>Prénom : </label><input type="text" name="prenom" placeholder="Saisir son prénom" className="form-control" autocomplete="off" />
+          <label>Nom : </label><input type="text" name="nom" placeholder="Saisir son nom" className="form-control" autocomplete="off"  />
+          <label>Email : </label><input type="text" name="email" placeholder="Saisir son e-mail" className="form-control" autocomplete="off" />
+          <label>Password : </label><input type="password" name="password" placeholder="Saisir son mot de passe " className="form-control" autocomplete="off" />
           <button type="button">S'inscrire </button>
           </form>
       </div>
