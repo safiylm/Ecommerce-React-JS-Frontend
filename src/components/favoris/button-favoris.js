c28c01b6e0e1aacda5c9b71e5c264e0745a692e3
safiylm <121391404+safiylm@users.noreply.github.com>
2024-06-04 11:19:59 +0200

    update my dislike function

--- src/components/favoris/button-favoris.js
+++ src/components/favoris/button-favoris.js
@@ -2,7 +2,7 @@ import { useState } from "react"
 
 
 function ButtonFavoris({ produitId }) {
-    const [isLiked, setIsLiked] = useState(JSON.parse(localStorage.getItem('favoris')).filter(f => f == produitId).length > 0);
+    const [isLiked, setIsLiked] = useState(JSON.parse(localStorage.getItem('favoris')).filter(f => f === produitId).length > 0);
     const like_ = () => {
         let fav_ = JSON.parse(localStorage.getItem('favoris'))
         fav_.push(produitId)
@@ -13,7 +13,7 @@ function ButtonFavoris({ produitId }) {
     const dislike_ = () => {
 
         let fav_ = JSON.parse(localStorage.getItem('favoris'))
-        fav_.filter(a =>
+        fav_ = fav_.filter(a =>
             a !== produitId
         )
         localStorage.setItem('favoris', JSON.stringify(fav_))
@@ -26,12 +26,15 @@ function ButtonFavoris({ produitId }) {
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" color="pinkhot" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" />
                     </svg>
+                   
                 </button>
                 :
                 <button id="btnAddHeart" onClick={like_}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" color="pinkhot" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
                         <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
-                    </svg> </button>
+                    </svg>
+                    </button>
+                     
             }</div>
     )
 }
